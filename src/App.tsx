732ad1df90d9dc8150bf2e8b38be7bff2acd096d
yuiseki <yuiseki@gmail.com>
2024-09-15 17:44:10 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -137,6 +137,51 @@ function App() {
     [midiOutputs]
   );
 
+  const ledLightning = useCallback(
+    (params: {
+      // Lightning Type, 0: Static, 1: Flashing, 2: Pulsing, 3: RGB
+      type?: number;
+      // LED index, must be between 11 and 99
+      index?: number;
+      // Index color, must be between 1 and 127
+      color?: number;
+      // Start index color
+      startColor?: number;
+      // End index color
+      endColor?: number;
+      // RGB color
+      red?: number;
+      green?: number;
+      blue?: number;
+    }) => {
+      if (!midiOutputs.length) {
+        console.error("No MIDI outputs available");
+        return;
+      }
+      const { type = 0, index = 36, color = 127 } = params;
+      const output = midiOutputs[0];
+      const header = dictionary.sysEx.header;
+      const footer = dictionary.sysEx.footer;
+      const command = dictionary.commands.ledLightning;
+      const typeValue = type;
+      if (typeValue === 0 || typeValue === 2) {
+        const colorValue = color;
+        const indexValue = index;
+        const message = new Uint8Array([
+          ...header,
+          command,
+          typeValue,
+          indexValue,
+          colorValue,
+          ...footer,
+        ]);
+        output.send(message);
+        return;
+      }
+    },
+    [midiOutputs]
+  );
+
   return (
     <>
       <div
@@ -157,12 +202,22 @@ function App() {
           onClick={() =>
             textScrolling({
               text: "unko",
-              loop: true,
+              loop: false,
               speed: 3,
             })
           }
         >
           unko
+        </button>
+        <button
+          onClick={() =>
+            ledLightning({
+              index: 11,
+              type: 2,
+            })
+          }
+        >
+          light button
         </button>
       </div>
     </>
